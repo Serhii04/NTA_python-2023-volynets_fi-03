437d1c88239b4dd200a1e7902aefe7d4a7a1931f
Serhii04 <fi.03.volinec.sergij@gmail.com>
2023-03-12 17:20:32 +0200

    add main function to run main algorithm

--- src/main.py
+++ src/main.py
@@ -1,7 +1,23 @@
 import nta_algorithms as nalg
 
 def main():
-    pass
+    while True:
+        n = input("Type your number: ")
+        if n.isnumeric():
+            n = int(n)
+            factor_n = nalg.get_canon_number_composition(n)
+
+            if len(factor_n):
+                print(f"n = {factor_n[0]}", end="")
+                
+                for d in range(1, len(factor_n)):
+                    print(f" * {factor_n[d]}", end="")
+                print("")
+            
+            return
+        else:
+            print(f"{n} isn't a number, please try again.")
+            
 
 
 if __name__ == "__main__":
