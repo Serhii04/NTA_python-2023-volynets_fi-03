bd97152df54eda7225ade3e5ab204e489a4bccb7
Serhii04 <fi.03.volinec.sergij@gmail.com>
2023-05-18 21:16:11 +0300

    update main

--- src/main.py
+++ src/main.py
@@ -1,22 +1,33 @@
-import nta_algorithms as nalg
+import nta_algorithms_lab_2 as nalg
+from nta_algorithms_lab_2 import time_limit
+import my_timer
+import signal
 
 def main():
-    while True:
-        n = input("Type your number: ")
-        if n.isnumeric():
-            n = int(n)
-            factor_n = nalg.get_canon_number_composition(n)
 
-            if len(factor_n):
-                print(f"n = {factor_n[0]}", end="")
-                
-                for d in range(1, len(factor_n)):
-                    print(f" * {factor_n[d]}", end="")
-                print("")
-            
-            return
-        else:
-            print(f"{n} isn't a number, please try again.")
+    print("To solve a^x = b (mod p), type next coeficients:")
+
+    timeout = 60*5
+
+    alpha = int(input("Type a value: a = "))
+    beta = int(input("Type b value: b = "))
+    p = int(input("Type p value: p = "))
+
+    rez = None
+    timer = my_timer.My_Timer()
+    try:
+        with time_limit(timeout):
+            rez = nalg.SPG(alpha=alpha, beta=beta, n=p - 1)
+    except UserWarning as e:
+        print("Timed out!")
+    
+    if rez:
+        # print(f"{alpha}^x = {beta} (mod {n+1})")
+        print(f"x = {rez}, spend: {timer.now():0.4f}s")
+    else:
+        print(f"No result, spend: {timer.now():0.4f}s")
+
+    return rez
             
 
 
