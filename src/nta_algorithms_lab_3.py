2c246fa538ca10faf0e66bcb115d4eb006349436
Serhii04 <fi.03.volinec.sergij@gmail.com>
2023-05-23 13:08:12 +0300

    Create file for lab functions

---
+++ src/nta_algorithms_lab_3.py
@@ -0,0 +1,17 @@
+import math
+
+# import nta_algorithms_lab_1 as lab_1
+# import nta_algorithms_lab_2 as lab_2
+# import my_timer
+
+
+
+######################################
+# Example
+######################################
+
+def main():
+    print("Welcome")
+
+if __name__ == "__main__":
+    main()
